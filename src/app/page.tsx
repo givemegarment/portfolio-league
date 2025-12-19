1f2ec10bc5f930f91444b804755db71c8b2edb36
givemegarment <givemegarments@gmail.com>
2025-12-19 09:08:22 +0300

    Refactor LeagueCard component: streamline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import { Metadata } from 'next';
 export const dynamic = 'force-dynamic';
 import NextDynamic from 'next/dynamic';
+
+export const metadata: Metadata = {
+  other: {
+    'base:app_id': '6943dcf2d19763ca26ddc3fb',
+  },
+};
 
 const HomeClient = NextDynamic(() => import('./home-client'), { ssr: false });
 
